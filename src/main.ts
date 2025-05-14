4df5c1479ca38f401c42db7ab227b769a11f6ac5
Eduard Vorsin <eduardvorsin2019@gmail.com>
2025-05-14 08:45:27 +0500

    [chore]: set up primevue

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,31 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import ToastService from 'primevue/toastservice';
+import Ripple from 'primevue/ripple';
 
 const pinia = createPinia();
 const app = createApp(App);
 
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.dark',
+      cssLayer: {
+        name: 'primevue',
+        order: 'theme, base, primevue',
+      }
+    }
+  },
+  ripple: true
+});
+
+app.directive('ripple', Ripple);
+
+app.use(ToastService);
 app.use(pinia);
 
 app.mount('#app');
-
-
