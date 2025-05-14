d310718e8e02dd0d65a5c0e2606c86511a19dae3
Eduard Vorsin <eduardvorsin2019@gmail.com>
2025-05-14 09:12:32 +0500

    [feat]: add TodoList component

---
+++ src/components/TodoList/TodoList.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import type { ITodo } from '@/services/api';
+import TodoItem from '../TodoItem/TodoItem.vue';
+
+defineProps<{ todos: ITodo[] }>();
+</script>
+
+<template>
+  <ul v-if="todos.length" class="mb-6">
+    <TodoItem v-for="todo in todos" :key="todo.id" :text="todo.text" :id="todo.id" :status="todo.status"
+      :date="todo.date" class="not-last:mb-2" />
+  </ul>
+  <div v-else class="text-center text-2xl dark:text-slate-200 text-slate-800 mb-8">
+    <p>Список задач пуст</p>
+  </div>
+</template>
